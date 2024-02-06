bcac92af7eb35f00f35969bd243f1c22015acaed
mernoult <41010884@parisnanterre.fr>
2024-02-06 17:22:56 +0000

    correction appli5

--- main.py
+++ main.py
@@ -4,64 +4,74 @@ Prediction de la survie d'un individu sur le Titanic
 
 # GESTION ENVIRONNEMENT --------------------------------
 
+import os
 import argparse
+import yaml
+import pandas as pd
+import matplotlib.pyplot as plt
 
-from import_data import import_yaml_config, import_data
-from build_features import (
-    create_variable_title,
-    fill_na_titanic,
-    label_encoder_titanic,
-    check_has_cabin,
-    ticket_length,
-)
-from train_evaluate import random_forest_titanic
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
 
+import import_data as imp
+import build_features as bf
+import train_evaluate as te
 
-parser = argparse.ArgumentParser(description="Paramètres du random forest")
-parser.add_argument("--n_trees", type=int, default=20, help="Nombre d'arbres")
-args = parser.parse_args()
 
 
 # PARAMETRES -------------------------------
 
-config = import_yaml_config("config.yaml")
+# Paramètres ligne de commande
+parser = argparse.ArgumentParser(description="Paramètres du random forest")
+parser.add_argument(
+    "--n_trees", type=int, default=20, help="Nombre d'arbres"
+)
+args = parser.parse_args()
 
+# Paramètres YAML
+config = imp.import_yaml_config("config.yaml")
 API_TOKEN = config.get("jeton_api")
 LOCATION_TRAIN = config.get("train_path", "train.csv")
 LOCATION_TEST = config.get("test_path", "test.csv")
-TEST_FRACTION = config.get("test_fraction", 0.1)
+TEST_FRACTION = config.get("test_fraction", .1)
 N_TREES = args.n_trees
 
 
 # FEATURE ENGINEERING --------------------------------
 
-TrainingData = import_data(LOCATION_TRAIN)
-TestData = import_data(LOCATION_TEST)
+TrainingData = imp.import_data(LOCATION_TRAIN)
+TestData = imp.import_data(LOCATION_TEST)
 
 # Create a 'Title' variable
-TrainingData = create_variable_title(TrainingData)
-TestData = create_variable_title(TestData)
+TrainingData = bf.create_variable_title(TrainingData)
+TestData = bf.create_variable_title(TestData)
 
 
-# IMPUTATION DES VARIABLES ================
+## IMPUTATION DES VARIABLES ================
 
-TrainingData = fill_na_titanic(TrainingData)
-TestData = fill_na_titanic(TestData)
 
-TrainingData = label_encoder_titanic(TrainingData)
-TestData = label_encoder_titanic(TestData)
+TrainingData = bf.fill_na_titanic(TrainingData)
+TestData = bf.fill_na_titanic(TestData)
+
+TrainingData = bf.label_encoder_titanic(TrainingData)
+TestData = bf.label_encoder_titanic(TestData)
 
 
 # Making a new feature hasCabin which is 1 if cabin is available else 0
-TrainingData = check_has_cabin(TrainingData)
-TestData = check_has_cabin(TestData)
+TrainingData = bf.check_has_cabin(TrainingData)
+TestData = bf.check_has_cabin(TestData)
 
-TrainingData = ticket_length(TrainingData)
-TestData = ticket_length(TestData)
+TrainingData = bf.ticket_length(TrainingData)
+TestData = bf.ticket_length(TestData)
+
 
 
 # MODELISATION: RANDOM FOREST ----------------------------
 
-model = random_forest_titanic(
-    data=TrainingData, fraction_test=TEST_FRACTION, n_trees=N_TREES
+model = te.random_forest_titanic(
+    data=TrainingData,
+    fraction_test=TEST_FRACTION,
+    n_trees=N_TREES
 )
