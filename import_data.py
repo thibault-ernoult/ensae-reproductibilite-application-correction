c441f13269eed80dafca4fb0f6db0300fe3b04cc
mernoult <41010884@parisnanterre.fr>
2024-02-06 16:38:39 +0000

    ajout script import_data.py

---
+++ import_data.py
@@ -0,0 +1,36 @@
+"""
+Import des paramètres et des données
+"""
+import os
+import yaml
+import pandas as pd
+
+
+def import_yaml_config(filename: str = "toto.yaml") -> dict:
+    """Import config.yaml
+
+    Args:
+        filename (str, optional): .yaml config file. Defaults to "toto.yaml".
+
+    Returns:
+        dict: config parameters in a Python dictionnary
+    """
+    dict_config = {}
+    if os.path.exists(filename):
+        with open(filename, "r", encoding="utf-8") as stream:
+            dict_config = yaml.safe_load(stream)
+    return dict_config
+
+
+def import_data(path: str) -> pd.DataFrame:
+    """Import Titanic datasets
+    Args:
+        path (str): File location
+    Returns:
+        pd.DataFrame: Titanic dataset
+    """
+
+    data = pd.read_csv(path)
+    data = data.drop(columns="PassengerId")
+
+    return data
